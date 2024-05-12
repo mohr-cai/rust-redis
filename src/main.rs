1ba542fb256771718c7fd80a6daa735207ce255b
zzfab <fabio.gruschka@gmail.com>
2024-05-12 09:00:40 +0200

    stage 5

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,8 @@ async fn handle_conn(stream: TcpStream) {
         let response = if let Some(v) = value {
             let (command, args) = extract_command(v).unwrap();
             match command.as_str() {
-                "ping" => Value::SimpleString("PONG".to_string()),
-                "echo" => args.first().unwrap().clone(),
+                "PING" => Value::SimpleString("PONG".to_string()),
+                "ECHO" => args.first().unwrap().clone(),
                 c => panic!("Cannot handle command {}", c),
             }
         } else {
